394b5172f43fb3848baaa52ca4e293466b5940ca
bi wei <wei_bi@126.com>
2017-05-17 20:47:29 +0800

    add the function of input to dir_p.c

--- lin_p/dir_p.c
+++ lin_p/dir_p.c
@@ -43,9 +43,13 @@ void printdir(char *dir, int depth){
 	closedir(dp);
 }
 
-int main(){
-	printf("Directory scan of /home/bv_user/Documents:\n");
-	printdir("/home/bv_user/Documents",0);
+int main(int argc, char* argv[]){
+	char *topdir = ".";
+	if(argc >=2 ) 
+		topdir = argv[1];
+
+	printf("Directory scan of %s\n",topdir);
+	printdir(topdir,0);
 	printf("done.\n");
 
 	exit(0);
